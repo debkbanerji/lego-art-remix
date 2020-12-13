4e1c2131ef9f42584869954d8a987dcd0270861b
deb <debkbanerji@gmail.com>
2020-12-12 18:19:01 -0800

    Update Version

--- app/js/index.js
+++ app/js/index.js
@@ -1,4 +1,4 @@
-const VERSION_NUMBER = "v2020.12.11";
+const VERSION_NUMBER = "v2020.12.12";
 document.getElementById("version-number").innerHTML = VERSION_NUMBER;
 
 // TODO: Display these values at the top of the page if they are large enough
