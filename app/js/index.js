5f6d3f2c7a397bcb3254ef90bbeaf1f0156ed046
deb <debkbanerji@gmail.com>
2020-12-31 15:20:34 -0800

    Export Studs Button

--- app/js/index.js
+++ app/js/index.js
@@ -1,4 +1,4 @@
-const VERSION_NUMBER = "v2020.12.28";
+const VERSION_NUMBER = "v2020.12.31";
 document.getElementById("version-number").innerHTML = VERSION_NUMBER;
 
 // TODO: Display these values at the top of the page if they are large enough
