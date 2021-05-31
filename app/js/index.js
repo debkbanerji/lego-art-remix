f6001d3c042cdf9cd58717ea09f8d4e933efe50e
deb <debkbanerji@gmail.com>
2021-05-30 18:21:02 -0700

    Basic Usage Metrics

--- app/js/index.js
+++ app/js/index.js
@@ -1,4 +1,4 @@
-const VERSION_NUMBER = "v2021.5.28";
+const VERSION_NUMBER = "v2021.5.30";
 document.getElementById("version-number").innerHTML = VERSION_NUMBER;
 
 // TODO: Display these values at the top of the page if they are large enough
---
+++ app/js/metrics.js
@@ -0,0 +1,45 @@
+const TS_ROUND_GRANULARITY = 1000;
+
+try {
+    if (window.location.href.match("metric")) {
+        perfLoggingDatabase = firebase.database();
+        perfLoggingDatabase
+            .ref("/input-image-count/per-day")
+            .once("value")
+            .then(snapshot => {
+                document.getElementById("metrics-card").hidden = false;
+                const val = snapshot.val();
+                if (val != null) {
+                    const mergedTSMap = {};
+                    Object.keys(val).forEach(rawTS => {
+                        const roundedTS =
+                            Math.round(rawTS / TS_ROUND_GRANULARITY) *
+                            TS_ROUND_GRANULARITY;
+                        mergedTSMap[roundedTS] =
+                            (mergedTSMap[roundedTS] || 0) + Number(val[rawTS]);
+                    });
+                    const dataPoints = Object.keys(mergedTSMap).map(ts => {
+                        return {ts: ts, count: mergedTSMap[ts]};
+                    });
+                    dataPoints.sort((d1, d2) => d2.ts - d1.ts);
+                    dataPoints.forEach(point => {
+                        const row = document.createElement("tr");
+                        const cell1 = document.createElement("td");
+                        cell1.innerHTML = new Date(
+                            point.ts * 1000
+                            // use year.month.day.
+                        ).toLocaleDateString("ko-KR");
+                        const cell2 = document.createElement("td");
+                        cell2.innerHTML = point.count;
+                        row.appendChild(cell1);
+                        row.appendChild(cell2);
+                        document
+                            .getElementById("input-image-count-table")
+                            .appendChild(row);
+                    });
+                }
+            });
+    }
+} catch (_e) {
+    // we don't care if this fails
+}
